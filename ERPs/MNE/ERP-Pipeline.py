a0de3cc289ea1c29e12b5fa7269e6254388adc43
Your Name <deckerwill7@gmail.com>
2023-01-19 20:07:36 -0600

    Typo corrections

--- ERPs/MNE/ERP-Pipeline.py
+++ ERPs/MNE/ERP-Pipeline.py
@@ -44,7 +44,7 @@ print(type(data))  # <class 'numpy.ndarray'>
 print(data.shape)  # (n_channels, n_samples)
 
 
-## Identifying Trigger Codes and epoching data
+## Identifying Trigger Codes and Epoching data
 
 events,_ = mne.events_from_annotations(raw)
 print(events[:]) # check events
